93e359997c7694502d2c0720583949ae0ec5698f
Adam <adam.iona@alumni.unc.edu>
2020-03-10 20:09:34 -0400

    Add scripts

---
+++ static/js/app.js
@@ -0,0 +1,30 @@
+// Read in JSON file
+//const data = await d3.json("/static/data/samples.json")
+d3.json("/static/data/samples.json").then(function(data) {
+    console.log(data);
+
+});
+
+// Create horizontal bar chart to display top 10 OTU
+
+ var trace1 = {
+    x: [0,50,100,150],
+    y: data.sample_values,
+    name: 'Top 10 OTU',
+    orientation: 'h',
+    marker: {
+        color: rgb(55,128,191),
+        width: 1
+    },
+    type: 'bar'
+}; 
+
+// Create bubble chart to display each sample
+
+// Display sample metadata
+
+// Display key-value pairs from metadata JSON
+
+// Update plots when new sample is selected
+
+    //Event handler?
