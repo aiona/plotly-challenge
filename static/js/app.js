9eab6b52bec89a1e98cd5046629616ea2342432e
Adam <adam.iona@alumni.unc.edu>
2020-03-11 22:23:10 -0400

    Adding Plots

--- static/js/app.js
+++ static/js/app.js
@@ -1,56 +1,75 @@
+function init() {
 
-
-
-// Create horizontal bar chart to display top 10 OTU
-
- /* var trace1 = {
-    x: data.sample_values,
-    y: data.otu_ids,
-    name: 'Top 10 OTU',
-    orientation: 'h',
-    marker: {
-        color: rgb(55,128,191),
-        width: 1
-    },
-    type: 'bar'
-};  */
-
-// Create bubble chart to display each sample
-
-// Display sample metadata
-
-// Display key-value pairs from metadata JSON
-
-// Update plots when new sample is selected
-
-    //Event handler?
-
-// Bonus
-function buildMetadata(sample) {
+    // Read in JSON file
+        d3.json("samples.json").then((data) => {
+            console.log(data);
+            
+           var names_list = data.names;
+        
+        });
     
-}
-
 function chart(sample) {
+
     d3.json("samples.json").then((data) => {
         var samples_list = data.samples;
 
-        var results = samples_list.filter(sample_item => sample_item.id == sample.id);
-        console.log(results);
-    });
-}
+        var resultArray = samples_list.filter(sample_item => sample_item.id == sample.id);
+        var result = resultArray[0];
+        console.log(result);
 
-function init() {
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
 
-// Read in JSON file
-    d3.json("samples.json").then((data) => {
-        console.log(data);
-        
-       var names_list = data.names;
-    
-    });
+        // Create horizontal bar chart to display top 10 OTU
+        var bars = {
+            x: sample_values,
+            y: otu_ids,
+            name: 'Top 10 OTU',
+            orientation: 'h',
+            marker: {
+            color: rgb(55,128,191),
+            width: 1
+            },
+            type: 'bar'
+        };
+
+        var dbars = bars;
+
+        Plotly.newPlot('bar', dbars);
+
+        // Create bubble chart to display each sample
+        var bubbles = {
+            x: otu_ids,
+            y: sample_values,
+            name: 'OTU ID',
+            mode: 'markers',
+            marker: {
+                color: otu_ids,
+            size: sample_values,
+            labels: otu_labels
+            }
+            // text:
+        };
+
+        var dbubbles = bubbles;
+
+        Plotly.newPlot('bubble', dbubbles);
+            
+
+    }
+)};
 
     var firstSample = names_list[0];
 
-    // buildMetadata(firstSample)
     chart(firstSample);
+
+// Display sample metadata    
+/*  function buildMetadata(firstSample) {
+    
+    } */
+
+    // Function to tie data.names to div id="well" (Test Subject ID)
+    // On change, show selected name in div class="panel-heading" (Demographic Info) by calling chart function
+    // Call chart function
 }
