c6fe72fc3996585054877b13f650ab5c649edfa0
Adam <adam.iona@alumni.unc.edu>
2020-03-23 15:11:16 -0400

    Update Scripts

--- static/js/app.js
+++ static/js/app.js
@@ -1,50 +1,89 @@
 function init() {
 
+    // Reference the dropdown select element
+    var selector = d3.select("#selDataset");
+
     // Read in JSON file
     d3.json("./static/js/samples.json").then((data) => {
         console.log(data);
         
         var names_list = data.names;
+
+        names_list.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample)
+        });
+
+        console.log(names_list)
         const firstSample = names_list[0];
         chart(firstSample);
-        
+        namesList = names_list;
+
+        buildMetadata(firstSample);
+         
     });
     
 }
 
+function buildMetadata(sample) {
+    d3.json("./static/js/samples.json").then((data) => {
+        var metadata = data.metadata;
+
+        console.log(metadata);
+        var resultArray = metadata.filter(metadata_item => metadata_item.id == sample);
+        var result = resultArray[0];
+        console.log(result);
+
+
+        var panel = d3.select("#sample-metadata");
+        panel.html("");
+
+        Object.entries(result).forEach(([key, value]) => {
+            panel.append("h6").text(`${key}: ${value}`);
+            
+        })
+    }
     
+    )
+}
+
 function chart(sample) {
 
     d3.json("./static/js/samples.json").then((data) => {
         var samples_list = data.samples;
 
-        var resultArray = samples_list.filter(sample_item => sample_item.id == sample.id);
+        console.log(samples_list);
+        var resultArray = samples_list.filter(sample_item => sample_item.id == sample);
         var result = resultArray[0];
         console.log(result);
 
         var otu_ids = result.otu_ids;
         var otu_labels = result.otu_labels;
-        var sample_values = result.sample_values;
+        var sample_values = result.sample_values.slice(0,10).reverse();
         console.log(otu_ids);
+        var top_otu = otu_ids.slice(0, 10).reverse()
+        console.log(top_otu)
 
         // Create horizontal bar chart to display top 10 OTU
         var bars = [{
             x: sample_values,
-            y: otu_ids,
+            y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+            orientation: 'h',
+            text: otu_labels.slice(0,10).reverse(),
             type: 'bar'
         }];
 
         var layout = {
-            title: 'title',
-            xaxis: { title: 'x-title'},
-            yaxis: { title: 'y-title'}
+            title: 'Top 10 OTUs',
         };
 
-
+        barsT = bars
         Plotly.newPlot('bar', bars, layout);
 
         // Create bubble chart to display each sample
-        /*
+       
         var bubbles = {
             x: otu_ids,
             y: sample_values,
@@ -52,12 +91,14 @@ function chart(sample) {
             mode: 'markers',
             marker: {
                 color: otu_ids,
-            size: sample_values,
+                size: sample_values,
             },
             text: otu_labels
+            //colorscale
         };
 
         var layout = {
+            //margins
 
             xaxis: {
                 title: 'OTU ID'
@@ -66,59 +107,20 @@ function chart(sample) {
         };
 
         var dbubbles = [bubbles];
+        bubblesT = dbubbles
 
-        Plotly.newPlot('bubble', dbubbles, layout);*/
+        Plotly.newPlot('bubble', dbubbles, layout);
             
 
     });
 }
 
-init()
+function optionChanged(newSample) {
 
-// Display sample metadata    
-/*  function buildMetadata(firstSample) {
-    
-    } 
+    buildMetadata(newSample);
 
-    // Populate dropdown menu with data.names - select id="well" (Test Subject ID)
-    
-    d3.selectAll("#selDataset").on("change", buildMetadata);
+    chart(newSample);
 
-    // Function called by change to DOM
-    function buildMetadata(newSample) {
+};
 
-        // Select dropdown menu and assign to variable
-        var dropdownMenu = d3.select("#selDataset");
-
-        // Assign the value of the dropdown menu option to a variable
-        var menuOption = dropdownMenu.property("value");
-
-        var data = [];
-
-        // Choose selected dataset based on value in names_list
-        if (data.names == firstSample) {
-            chart(firstSample);
-        }
-
-        else if (data.names != firstSample) {
-            chart(sample);
-        }
-
-        // Update the restyled plot's values
-        function updatePlotly(newdata) {
-            Plotly.restyle("bar", "values", [data]);
-        }
-    }
-
-
-
-        // Call function to update the chart
-        updatePlotly(data);
-    
-   
-
-
-    // On change, show selected name in div class="panel-heading" (Demographic Info) by calling chart function
-    // Call chart function
-}
-*/
+    init()
